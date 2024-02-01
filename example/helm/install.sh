43644be9463234495aad63c161166a6e41d1d688
Krzysztof Romanowski <krzysztof.romanowski.kr3@roche.com>
2024-02-01 19:26:07 +0000

    add values for oauth2-proxy, istiod, add CRB for anonymous requests to the /.well-known/openid-configuration

--- example/helm/install.sh
+++ example/helm/install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -ex
 
+kubectl apply -f example/helm/clusterrolebinding.unauthenticated-oidc-viewer.yaml
+
 kubectl create ns istio-system || true
 kubectl create ns istio-ingress || true
+kubectl create ns oauth2-proxy || true
 kubectl create ns kubeflow || true
 kubectl label namespace kubeflow istio-injection=enabled --overwrite
+
+helm upgrade --install \
+	oauth2-proxy \
+	oauth2-proxy \
+	--repo https://oauth2-proxy.github.io/manifests \
+	--values example/helm/values.oauth2-proxy.yaml \
+	--kube-context vcluster_romanok1-kubeflow_vcluster-romanok1-kubeflow_uat-context \
+	--namespace oauth2-proxy
 
 helm upgrade --install \
 	istio-base \
@@ -13,10 +24,12 @@ helm upgrade --install \
 	--kube-context vcluster_romanok1-kubeflow_vcluster-romanok1-kubeflow_uat-context \
 	--namespace istio-system
 
+# Add mesh config for oauth2-proxy!
 helm upgrade --install --wait \
 	istiod \
 	istiod \
 	--repo https://istio-release.storage.googleapis.com/charts \
+	--values example/helm/values.istiod.yaml \
 	--kube-context vcluster_romanok1-kubeflow_vcluster-romanok1-kubeflow_uat-context \
 	--namespace istio-system
 
