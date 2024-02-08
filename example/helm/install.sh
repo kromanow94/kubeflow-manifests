c622db7d45c0c2e9209f27110190286c3a53b44d
Krzysztof Romanowski <krzysztof.romanowski.kr3@roche.com>
2024-02-08 09:35:24 +0000

    dex integration

--- example/helm/install.sh
+++ example/helm/install.sh
@@ -3,11 +3,20 @@ set -ex
 
 kubectl apply -f example/helm/clusterrolebinding.unauthenticated-oidc-viewer.yaml
 
+kubectl create ns dex || true
+kubectl create ns oauth2-proxy || true
 kubectl create ns istio-system || true
 kubectl create ns istio-ingress || true
-kubectl create ns oauth2-proxy || true
 kubectl create ns kubeflow || true
 kubectl label namespace kubeflow istio-injection=enabled --overwrite
+
+helm upgrade --install \
+	dex \
+	dex \
+	--repo https://charts.dexidp.io \
+	--values example/helm/values.dex.yaml \
+	--kube-context vcluster_romanok1-kubeflow_vcluster-romanok1-kubeflow_uat-context \
+	--namespace dex
 
 helm upgrade --install \
 	oauth2-proxy \
@@ -45,3 +54,23 @@ helm upgrade --install \
 	charts/kubeflow \
 	--kube-context vcluster_romanok1-kubeflow_vcluster-romanok1-kubeflow_uat-context \
 	--namespace kubeflow
+
+
+kubectl apply -f example/helm/requestauthentication.yaml
+
+# here deploy job to place jwks in the RequestAuthentication
+# https://github.com/kubeflow/manifests/pull/2544/files
+# common/oidc-client/oauth2-proxy/components/configure-self-signed-kubernetes-oidc-issuer/script.sh
+kubectl apply -f example/helm/job.configure-self-signed-issuer.yaml
+
+# This should go to metacontroller
+kubectl apply -f example/helm/authorizationpolicy.yaml
+
+# NOTES!
+# ---
+# newly created notebook provide wrong gateway reference(missing namespace):
+#   gateways:
+#   - kubeflow-gateway
+# ---
+# Default ns-owner-access-istio doesn't allow access to notebook.
+# Deleting it helps.
