d161007a3bb7956ba87c4e7ef7953640145175ed
Krzysztof Romanowski <krzysztof.romanowski.kr3@roche.com>
2024-06-12 18:39:10 +0200

    use ./tests/gh-actions/wait_for_pods_running_or_completed.sh

--- tests/gh-actions/install_pipelines.sh
+++ tests/gh-actions/install_pipelines.sh
@@ -7,5 +7,8 @@ echo "Waiting for crd/compositecontrollers.metacontroller.k8s.io to be available
 kubectl wait --for condition=established --timeout=30s crd/compositecontrollers.metacontroller.k8s.io
 kustomize build env/cert-manager/platform-agnostic-multi-user | kubectl apply -f -
 sleep 60
-kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout 600s
+
+# kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout 600s
+./tests/gh-actions/wait_for_pods_running_or_completed.sh --all-namespaces
+
 cd -
