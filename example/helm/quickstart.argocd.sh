b7158aa7bd87bbc841bc39aee78289d97423253d
Krzysztof Romanowski <krzysztof.romanowski.kr1@roche.com>
2025-03-31 21:55:21 +0000

    kubeflow-0.5.0

--- example/helm/quickstart.argocd.sh
+++ example/helm/quickstart.argocd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-TARGET_REVISION="${TARGET_REVISION:-kubeflow-0.5.0-rc.5}"
+TARGET_REVISION="${TARGET_REVISION:-kubeflow-0.5.0}"
 
 cat <<EOF
 This script will check if ArgoCD is installed, install ArgoCD if not installed
